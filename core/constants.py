543d63adef8567898e61754fe3bc62b5aacd035e
stormyoasis <goprotege@gmail.com>
2025-08-24 19:52:35 -0700

    Adding cmd line arguments to server

--- core/constants.py
+++ core/constants.py
@@ -1,2 +1,4 @@
 DEFAULT_TIMEOUT: int = 300  # Default timeout for client connections in seconds
 DEFAULT_ROOM: str = "General"
+DEFAULT_HOST: str = "127.0.0.1"
+DEFAULT_PORT: int = 12345
--- core/server.py
+++ core/server.py
@@ -1,10 +1,11 @@
 import socket
 import threading
-from core.constants import DEFAULT_TIMEOUT, DEFAULT_ROOM
+from core.constants import DEFAULT_TIMEOUT, DEFAULT_ROOM, DEFAULT_HOST, DEFAULT_PORT
 from data.message import Message
 from data.clientsession import ClientSession
 from data.room import Room
 from typing import List
+from argparse import ArgumentParser
 
 class ChatServer:
     # Chat server class for handling multiple client connections and message routing
@@ -23,5 +24,11 @@ class ChatServer:
         pass
     
 if __name__ == "__main__":
-    server = ChatServer()
+    parser = ArgumentParser(description="Start the server")
+    parser.add_argument("--host", default=DEFAULT_HOST, help="Host address")
+    parser.add_argument("--port", type=int, default=DEFAULT_PORT, help="Port number for server to listent on")
+    parser.add_argument("--timeout", type=int, default=DEFAULT_TIMEOUT, help="Timeout in seconds")
+    args = parser.parse_args()    
+    
+    server = ChatServer(args.host, args.port, args.timeout)
     server.start()
